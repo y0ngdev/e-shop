176c6e3b1087408f1c3fdb288a2d94095e5279ba
Prosper Ugbovo [YONGDEV] <85100001+prosperyong@users.noreply.github.com>
2024-09-10 13:31:26 +0100

    removr recommender completely

--- backend/settings.py
+++ backend/settings.py
@@ -113,7 +113,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 if not DEBUG:
     DATABASES = {
     	'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv('DB_NAME'),
             'USER': os.getenv('DB_USER'),
             'PASSWORD': os.getenv('DB_PASSWORD'),
@@ -121,13 +121,13 @@ if not DEBUG:
             'PORT': os.getenv('DB_PORT'),       # Default PostgreSQL port
     }
     }
-
-DATABASES = {
+if DEBUG:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    },
-}
+    }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
