81c1cf2fc718fc36245df74f70992af2d15e762d
Prosper Ugbovo [YONGDEV] <85100001+prosperyong@users.noreply.github.com>
2024-09-10 14:48:10 +0100

    fixes

--- backend/settings.py
+++ backend/settings.py
@@ -83,7 +83,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'app.middleware.SessionTimeoutMiddleware',
+    # 'app.middleware.SessionTimeoutMiddleware',
 ]
 
 
